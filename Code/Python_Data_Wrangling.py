bee201735feabbe39714afcb07dccc2b954681d7
mvvollmer <miles.vollmer@gmail.com>
2023-08-01 15:51:22 -0400

    New stuff

--- Code/Python_Data_Wrangling.py
+++ Code/Python_Data_Wrangling.py
@@ -6,6 +6,10 @@ data_path = '/Users/milesvollmer/Panama_Tank_Field/intermediate_files/normalized
 
 df = pd.read_csv(data_path)
 
+# Make sure doing this correctly
+genusdf = df.drop(['health', 'sample_id', 'cpm_norm', 'cpm', 'n_reads', 'lib.size', 'norm.factors', 'year', 'season', 'site', 'dataset', 'domain', 'phylum', 'class', 'order', 'family', 'log2_cpm_norm'], axis=1)
+genusdf = genusdf.groupby('asv_id').first()
+print(genusdf['genus'].value_counts())
 df = df.drop(['cpm_norm', 'cpm', 'n_reads', 'lib.size', 'norm.factors'], axis=1)
 df["health"] = np.where(df["health"] == "D", 1, 0)
 df['asv_id'] = df['asv_id'].astype(str)
@@ -13,6 +17,7 @@ df['family'] = df['family'].astype(str)
 df['genus'] = df['genus'].astype(str)
 ndf = df
 df['asv_id'] = df[['asv_id', 'family', 'genus']].agg('-'.join, axis=1)
+
 
 # sample as X axis, health and ASVs as Y
 sampledf = df.drop(['year', 'season', 'site', 'dataset', 'domain', 'phylum', 'class', 'order', 'family', 'genus'], axis=1)
@@ -22,6 +27,19 @@ healthdf = healthdf.groupby('sample_id').max()
 sampledf = sampledf.pivot(index= 'sample_id', columns= 'asv_id', values='log2_cpm_norm')
 fullSampledf = pd.concat([sampledf, healthdf], axis=1)
 
+# sample as X axis, health and Family as Y
+'''
+samplendf = ndf.drop(['year', 'season', 'site', 'dataset', 'domain', 'phylum', 'class', 'order', 'genus'], axis=1)
+samplendf = samplendf.pivot_table(index= 'sample_id', columns= 'family', values='log2_cpm_norm', aggfunc='sum')
+fullSampledf = pd.concat([fullSampledf, samplendf], axis=1)
+'''
+
+samplen1df = ndf.drop(['year', 'season', 'site', 'dataset', 'domain', 'phylum', 'class', 'order', 'family'], axis=1)
+samplen1df = samplen1df.pivot_table(index= 'sample_id', columns= 'genus', values='log2_cpm_norm', aggfunc='median')
+fullSampledf = pd.concat([fullSampledf, samplen1df], axis=1)
+
+fullSampledf = fullSampledf.drop('nan', axis =1)
+
 dataset = fullSampledf
 
 features = dataset.drop(['health'], axis=1).columns
@@ -29,21 +47,16 @@ target = ['health']
 y=dataset.loc[:, target]
 X=dataset.loc[:, features]
 
+
 X_train, X_test, y_train, y_test = train_test_split(X,              #the input features
                                                     y,              #the label
                                                     test_size=0.3,  #set aside 30% of the data as the test set
                                                     random_state=7, #reproduce the results
                                                    )
-ndf = ndf.drop(['year'], axis=1)
-healthyndf = ndf.where(ndf['health'] == 0)
-healthyndf = healthyndf.groupby('family').sum()
-healthyndf = healthyndf.rename(columns={'log2_cpm_norm' : 'healthy_log2_cpm_norm'})
-unhealthyndf = ndf.where(ndf['health'] == 1)
-unhealthyndf = unhealthyndf.groupby('family').sum()
-unhealthyndf = unhealthyndf.rename(columns={'log2_cpm_norm' : 'diseased_log2_cpm_norm'})
-ndf = pd.concat([healthyndf, unhealthyndf], axis=1)
-ndf = ndf.drop(['health'], axis=1)
-print(ndf.head())
+
+
+
+
 
 
 
--- Code/RF_Feat_IMP2.py
+++ Code/RF_Feat_IMP2.py
@@ -16,6 +16,57 @@ explainer = shap.TreeExplainer(rf)
 # Calculate SHAP values for the test data
 shap_values = explainer.shap_values(X_test)
 
+from sklearn.inspection import permutation_importance
+
+# Compute permutation feature importance
+result = permutation_importance(rf, X_test, y_test, n_repeats=10, random_state=42)
+perm_importance = result.importances_mean
+
+
+
+# Sort permutation feature importance values in descending order
+sorted_indices = perm_importance.argsort()[::-1]
+top_10_perm_importance = perm_importance[sorted_indices][:10]
+
+# Get the corresponding feature names for the top 10 importance values
+top_10_feature_names = X_test.columns[sorted_indices][:10]
+
+# Plot the top 10 permutation feature importance
+plt.figure()
+plt.barh(top_10_feature_names, top_10_perm_importance, color='skyblue')
+plt.xlabel('Permutation Feature Importance')
+plt.ylabel('Feature Names')
+plt.title('Top 10 Permutation Feature Importance')
+plt.gca().invert_yaxis()
+plt.subplots_adjust(left=.5)
+plt.show()
+
+gini_importance = abs(rf.coef_[0])
+# Sort Gini feature importance values in descending order
+gini_sorted_indices = gini_importance.argsort()[::-1]
+top_10_gini_importance = gini_importance[gini_sorted_indices][:10]
+
+# Get the corresponding feature names for the top 10 importance values
+top_10_feature_names = X_train.columns[gini_sorted_indices][:10]
+
+# Plot the top 10 Gini feature importance
+plt.figure()
+plt.barh(top_10_feature_names, top_10_gini_importance, color='lightcoral')
+plt.xlabel('Gini Feature Importance')
+plt.ylabel('Feature Names')
+plt.title('Top 10 Gini Feature Importance')
+plt.gca().invert_yaxis()
+plt.subplots_adjust(left=.5)
+plt.show()
+
+explainer = shap.Explainer(rf, X_train)
+shap_values = explainer(X_test)
+
+shap.plots.beeswarm(shap_values, show=False)
+fig, ax = plt.gcf(), plt.gca()
+plt.subplots_adjust(left=.5)
+plt.show()
+
 # Choose a specific data point from the test set (e.g., the first data point)
 data_point_index = 0
 
@@ -46,6 +97,13 @@ plt.ylabel("Features", labelpad=20)
 plt.title("Top 10 Feature Importance - Mean SHAP Values")
 plt.subplots_adjust(left=.5)
 plt.show()
+
+
+shap.plots.beeswarm(shap_values, show=False)
+fig, ax = plt.gcf(), plt.gca()
+plt.subplots_adjust(left=.5)
+plt.show()
+
 
 # Choose 10-15 instances from the test set for which you want to create the force plot
 selected_instances = X_test.iloc[:15]  # Change the slice to select desired instances
