549532c5ef49915e0278045c2ea20cc8ff13a1ba
Jason Selwyn <jdselwyn@gmail.com>
2024-05-17 10:43:21 -0400

    update

--- Code/utils/bocas_water_temp.R
+++
@@ -1,8 +0,0 @@
-
-library(tidyverse)
-library(lubridate)
-
-read_csv('../../Data/bocas_temp/bocas_tower_wt_elect.csv') %>%
-  select(datetime, wt, chk_note) %>%
-  mutate(datetime = mdy_hms(datetime),
-         year = year(datetime))
